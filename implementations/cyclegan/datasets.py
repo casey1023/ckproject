42bf81877421b8e4daa3a1a40da507ac71f2e1c1
casey1023 <57552255+casey1023@users.noreply.github.com>
2020-12-04 11:02:32 +0800

    1

--- implementations/cyclegan/datasets.py
+++ implementations/cyclegan/datasets.py
@@ -14,7 +14,7 @@ def to_rgb(image):
 
 
 class ImageDataset(Dataset):
-    def __init__(self, root, transforms_=None, unaligned=False, mode="train"):
+    def __init__(self, root, transforms_=None, unaligned=False):
         self.transform = transforms.Compose(transforms_)
         self.unaligned = unaligned
 
